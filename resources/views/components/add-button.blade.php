cb229b3c0694a9d0f124a9dd4894d4d3a80c838b
Gursharan Singh <gursharan@vsrkcapital.com>
2023-01-04 10:33:50 +0530

    remove every code

--- resources/views/components/add-button.blade.php
+++
@@ -1,3 +0,0 @@
-<button {{ $attributes->merge(['class' => 'addBtn inline-flex items-center px-4 py-2 bg-red-700 border border-transparent rounded-md font-semibold clearfix text-xs text-white uppercase tracking-widest hover:bg-red-500 active:bg-red-500 focus:outline-none focus:border-red-900 focus:ring ring-red-300 disabled:opacity-25 transition ease-in-out duration-150']) }}>
-    {{ $slot }}
-</button>
--- resources/views/components/application-logo.blade.php
+++
@@ -1,1 +0,0 @@
-<img src="{{asset('imgs/vsrk circle.png')}}" alt="logo" width="60">
